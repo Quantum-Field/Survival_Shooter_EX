d56c226ca40c07be349aa289d855fb6951e51cae
Quantum-Field <nukuornetuoyh.gmail.com>
2016-07-09 22:46:52 +0900

    add new project files

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+	public float restartDelay = 5f;
+
+    Animator anim;
+	float restartTimer;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+			this.restartTimer += Time.deltaTime;
+
+			if(this.restartTimer >= this.restartDelay)
+			{
+				Application.LoadLevel(Application.loadedLevel);
+			}
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+	private static int killEnemies;
+
+	Text text;
+
+	public static void AddScore(int score)
+	{
+		killEnemies++;
+		int bonus = 0;
+		if ((killEnemies % 10 == 0))
+		{
+			bonus = 100;
+		}
+		ScoreManager.score += (score + bonus);
+	}
+
+    void Awake ()
+    {
+        text = GetComponent <Text> ();
+        score = 0;
+		killEnemies = 0;
+    }
+
+    void Update ()
+    {
+        text.text = "Score: " + score;
+    }
+}
